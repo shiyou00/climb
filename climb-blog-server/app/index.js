5b34f5d0a53a250ed95a327fb50c716091f68aae
Frankshi <frankshi@easyops.cn>
2019-12-13 09:42:57 +0800

    feat(router): customer router

--- climb-blog-server/app/index.js
+++ climb-blog-server/app/index.js
@@ -1,9 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
-const routing = require('./routers/index');
 
-// 匹配路由
-routing(app);
+
+app.use(async (ctx)=>{
+  if(ctx.url === '/'){
+    ctx.body = '这是主页';
+  }else if(ctx.url === '/users'){
+    if(ctx.method === 'GET'){
+      ctx.body = '这是用户列表页';
+    }else if(ctx.method === 'POST'){
+      ctx.body = '创建用户';
+    }else{
+      ctx.status = 405;
+    }
+  }else if(ctx.url.match(/\/users\/\w+/)){
+    const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+    ctx.body = `这是用户的ID ${userId}`;
+  }
+  else{
+    ctx.status = 404;
+  }
+});
 
 // 监听端口
 app.listen(3000,()=>{
