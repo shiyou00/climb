7c8f96b13c08d175321f6bd1b10b0b23a0171f41
Frankshi <frankshi@easyops.cn>
2020-04-18 21:32:05 +0800

    feat(climb): 首页卡片展示

--- climb-blog-front/src/pages/app/App.js
+++ climb-blog-front/src/pages/app/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.scss';
-import { Button } from 'antd';
+import { Card } from 'antd';
 
 function App() {
+
+  const allCards = [
+    {
+      name:"个人简历",
+      url:"",
+      content:"网页版个人简历，欢迎查阅。"
+    }
+  ];
+
   return (
-    <div className="app">
-      <Button size="large">我是第一个界面</Button>
+    <div className="wrap">
+      <div className="app">
+        {
+          allCards.map((card,index)=>{
+            return (
+              <Card hoverable key={index} title={card.name} extra={<a href="#">详情</a>} style={{ width: 300, height: 200 }}>
+                {card.content}
+              </Card>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
